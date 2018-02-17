6b1bf5d5fea7ddc19839da76e2ce4475d457cc0b
Alex Bepple <alex@bepple.de>
2018-02-17 17:42:20 +0100

    . define component props

--- client/src/flakes/calories/CaloriesEntries.js
+++ client/src/flakes/calories/CaloriesEntries.js
@@ -2,10 +2,14 @@ import * as r from 'ramda'
 import * as React from 'react'
 import hh_ from 'hyperscript-helpers'
 const {p} = hh_(React.createElement)
+import * as ceT from './calories-entry-type'
+import {defineProps} from '../../util/types'
+
+const t = {p: defineProps('entries')}
 
 const renderEntry = entry =>
   p({key: entry.id}, r.join(' ')([entry.time.toString(), entry.description, entry.calories]))
 
 export const CaloriesEntries = props => {
-  return r.map(renderEntry, props.entries)
+  return r.map(renderEntry, props[t.p.entries])
 }
