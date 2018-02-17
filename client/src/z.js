e46cd0c0bced908bfebea61b75cc3147bce52e3c
Alex Bepple <alex@bepple.de>
2018-02-17 15:34:35 +0100

    . remove unused complexity

--- client/src/z.js
+++ client/src/z.js
@@ -1,15 +1,5 @@
 import s from 's-js'
 import * as r from 'ramda'
-import * as re from 'recompose'
 
 const signalLens = r.lens(s => s(), (val, s) => s(val))
 export const over = r.over(signalLens)
-
-export const connect = mapOwnProps => re.compose(
-  re.lifecycle({
-    componentDidMount() {
-      s(() => this.setState(mapOwnProps(this.props)))
-    }
-  }),
-  re.mapProps(mapOwnProps)
-)
