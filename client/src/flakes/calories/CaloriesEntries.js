34aea650b65804536ddcad2f17594d917eb8530e
Alex Bepple <alex@bepple.de>
2018-02-17 17:45:06 +0100

    . use calories-entry-type

--- client/src/flakes/calories/CaloriesEntries.js
+++ client/src/flakes/calories/CaloriesEntries.js
@@ -7,8 +7,8 @@ import {defineProps} from '../../util/types'
 
 const t = {p: defineProps('entries')}
 
-const renderEntry = entry =>
-  p({key: entry.id}, r.join(' ')([entry.time.toString(), entry.description, entry.calories]))
+const renderEntry = ce => p({key: ce.id}, r.join(' ')([
+  ceT.g.time(ce).toString(), ceT.g.description(ce), ceT.g.calories(ce)]))
 
 export const CaloriesEntries = props => {
   return r.map(renderEntry, props[t.p.entries])
