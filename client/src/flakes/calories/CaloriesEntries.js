c67c117e18dca29c9df4732f3f8035de288665fb
Alex Bepple <alex@bepple.de>
2018-02-17 16:59:56 +0100

    . use hyperscript-helpers

--- client/src/flakes/calories/CaloriesEntries.js
+++ client/src/flakes/calories/CaloriesEntries.js
@@ -1,8 +1,10 @@
 import * as r from 'ramda'
 import * as React from 'react'
+import hh_ from 'hyperscript-helpers'
+const {p} = hh_(React.createElement)
 
 const renderEntry = entry =>
-  <p key={entry.id}>{entry.time.toString()} {entry.description} {entry.calories}</p>
+  p({key: entry.id}, r.join(' ')([entry.time.toString(), entry.description, entry.calories]))
 
 export const CaloriesEntries = props => {
   return r.map(renderEntry, props.entries)
