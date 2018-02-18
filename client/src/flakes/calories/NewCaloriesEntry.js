0a30b085fbd8950f0c35e00fb1ac8cb659310173
Alex Bepple <alex@bepple.de>
2018-02-18 18:26:17 +0100

    . dry

--- client/src/flakes/calories/NewCaloriesEntry.js
+++ client/src/flakes/calories/NewCaloriesEntry.js
@@ -16,24 +16,25 @@ const { input, button } = hh(h)
 const t = { p: defineProps('onAdd') }
 
 const newEntry$ = s.data(ceT.create(new Date(), '', 0))
+const evolveNewEntry = r.curry((f, val) => z.evolve(f(val), newEntry$))
 
 export const NewCaloriesEntry = props =>
   autoKey([
     dtlInput({
       defaultValue: ceT.g.time(newEntry$()),
-      onChange: r.compose(z.evolve(r.__, newEntry$), ceT.s.time)
+      onChange: evolveNewEntry(ceT.s.time)
     }),
     input({
       type: 'text',
       defaultValue: ceT.g.description(newEntry$()),
-      onChange: e => z.evolve(ceT.s.description(e.target.value), newEntry$)
+      onChange: e => evolveNewEntry(ceT.s.description, e.target.value)
     }),
     input({
       style: { width: '5em' },
       type: 'number',
       defaultValue: ceT.g.kcal(newEntry$()),
       onChange: e =>
-        z.evolve(ceT.s.kcal(r.defaultTo(0, e.target.valueAsNumber)), newEntry$)
+        evolveNewEntry(ceT.s.kcal, r.defaultTo(0, e.target.valueAsNumber))
     }),
     button(
       {
