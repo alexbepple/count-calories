ca203dca19dbccaa0934f0acb09a85fb49a3545b
Alex Bepple <alex@bepple.de>
2018-02-18 17:28:42 +0100

    $ preset input elements to initial values

--- client/src/flakes/calories/NewCaloriesEntry.js
+++ client/src/flakes/calories/NewCaloriesEntry.js
@@ -2,6 +2,7 @@ import h from 'react-hyperscript'
 import hh from 'hyperscript-helpers'
 import s from 's-js'
 import * as r from 'ramda'
+import { DateTime } from 'luxon'
 
 import * as z from 'util/s-js'
 import { defineProps } from 'util/types'
@@ -16,6 +17,7 @@ const t = { p: defineProps('onAdd') }
 const newEntry$ = s.data(ceT.create(new Date(), '', 0))
 
 const dateTimeLocal = {
+  toValue: x => DateTime.fromJSDate(x).toFormat("yyyy-MM-dd'T'HH:mm"),
   getDateFromChangeEvent: e => new Date(e.target.valueAsNumber)
 }
 
@@ -23,16 +25,19 @@ export const NewCaloriesEntry = props =>
   autoKey([
     input({
       type: 'datetime-local',
+      defaultValue: r.compose(dateTimeLocal.toValue, ceT.g.time, newEntry$)(),
       onChange: e =>
         z.evolve(ceT.s.time(dateTimeLocal.getDateFromChangeEvent(e)), newEntry$)
     }),
     input({
       type: 'text',
+      defaultValue: ceT.g.description(newEntry$()),
       onChange: e => z.evolve(ceT.s.description(e.target.value), newEntry$)
     }),
     input({
       style: { width: '5em' },
       type: 'number',
+      defaultValue: ceT.g.kcal(newEntry$()),
       onChange: e =>
         z.evolve(ceT.s.kcal(r.defaultTo(0, e.target.valueAsNumber)), newEntry$)
     }),
