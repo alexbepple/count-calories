4006172d4ec55cd46a407c19d2661980826a649a
Alex Bepple <alex@bepple.de>
2018-02-18 16:32:42 +0100

    $ configure added entry

--- client/src/flakes/calories/NewCaloriesEntry.js
+++ client/src/flakes/calories/NewCaloriesEntry.js
@@ -1,6 +1,7 @@
 import h from 'react-hyperscript'
 import hh from 'hyperscript-helpers'
 import s from 's-js'
+import * as r from 'ramda'
 
 import * as z from 'util/s-js'
 import { defineProps } from 'util/types'
@@ -12,21 +13,28 @@ const { input, button } = hh(h)
 
 const t = { p: defineProps('onAdd') }
 
-const newEntry$ = s.data(ceT.create(new Date(), 'new', 6))
+const newEntry$ = s.data(ceT.create(new Date(), '', 0))
+
+const dateTimeLocal = {
+  getDateFromChangeEvent: e => new Date(e.target.valueAsNumber)
+}
 
 export const NewCaloriesEntry = props =>
   autoKey([
     input({
       type: 'datetime-local',
-      onChange: e => z.evolve(ceT.s.time(e.target.value), newEntry$)
+      onChange: e =>
+        z.evolve(ceT.s.time(dateTimeLocal.getDateFromChangeEvent(e)), newEntry$)
     }),
     input({
       type: 'text',
       onChange: e => z.evolve(ceT.s.description(e.target.value), newEntry$)
     }),
     input({
+      style: { width: '5em' },
       type: 'number',
-      onChange: e => z.evolve(ceT.s.kcal(e.target.value), newEntry$)
+      onChange: e =>
+        z.evolve(ceT.s.kcal(r.defaultTo(0, e.target.valueAsNumber)), newEntry$)
     }),
     button({ onClick: () => props[t.p.onAdd](newEntry$()) }, 'Add')
   ])
