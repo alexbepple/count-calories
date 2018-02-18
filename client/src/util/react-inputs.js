bb9ed793c11884d7436a66161235db59505fd2bc
Alex Bepple <alex@bepple.de>
2018-02-18 18:40:05 +0100

    . simplify, rename

--- client/src/util/react-inputs.js
+++ client/src/util/react-inputs.js
@@ -5,18 +5,17 @@ import hh from 'hyperscript-helpers'
 
 const { input } = hh(h)
 
-export const dateTimeLocal = {
+export const dtl = {
   toValue: r.pipe(
     x => DateTime.fromJSDate(x),
     r.ifElse(x => x.isValid, x => x.toFormat("yyyy-MM-dd'T'HH:mm"), x => null)
-  ),
-  getDateFromChangeEvent: e => new Date(e.target.valueAsNumber)
+  )
 }
 
 export const dtlInput = r.pipe(
   r.evolve({
-    defaultValue: dateTimeLocal.toValue,
-    onChange: f => e => f(dateTimeLocal.getDateFromChangeEvent(e))
+    defaultValue: dtl.toValue,
+    onChange: f => e => f(new Date(e.target.valueAsNumber))
   }),
   r.merge({ type: 'datetime-local' }),
   input
--- client/src/util/react-inputs.test.js
+++ client/src/util/react-inputs.test.js
@@ -1,11 +1,11 @@
 import { assertThat, is, string } from 'hamjest'
-import { dateTimeLocal } from './react-inputs'
+import { dtl } from './react-inputs'
 
 describe('datetime-local input', () => {
   it('accepts valid Date objects', () => {
-    assertThat(dateTimeLocal.toValue(new Date()), is(string()))
+    assertThat(dtl.toValue(new Date()), is(string()))
   })
   it('defaults to null', () => {
-    assertThat(dateTimeLocal.toValue(null), is(null))
+    assertThat(dtl.toValue(null), is(null))
   })
 })
