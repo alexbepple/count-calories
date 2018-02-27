8ff4818ea01807fe48d11a4d6dfb097868a6e715
Alex Bepple <alex@bepple.de>
2018-02-27 18:21:21 +0100

    $ serve fake entries from Webtask

--- webtask/index.js
+++ webtask/index.js
@@ -2,7 +2,11 @@ const app = new (require("express"))();
 const wt = require("webtask-tools");
 
 app.get("/", function(req, res) {
-  res.end("Hello, world!");
+  const entries = [
+    { id: "rkXObM7Of", datetime: new Date(), description: "foo", kcal: 5 },
+    { id: "BkemuWfmuM", datetime: new Date(), description: "bar", kcal: 11 }
+  ];
+  res.json(entries);
 });
 
 module.exports = wt.fromExpress(app);
