cab9a4218d5992e17b696a9f49dd85ade6892050
Alex Bepple <alex@bepple.de>
2018-02-28 19:01:33 +0100

    . dry

--- client/src/flakes/http/index.js
+++ client/src/flakes/http/index.js
@@ -1,22 +1,29 @@
 import * as r from 'ramda'
+import { argsToArray } from 'util/generic'
 import { authToken$ } from 'flakes/auth'
 
 const getFullResourceName = r.concat('/api')
 
+const auth = () => ({ Authorization: 'Bearer ' + authToken$() })
+const sendJson = {
+  contentType: { 'Content-Type': 'application/json' }
+}
+const headers = r.pipe(
+  argsToArray,
+  r.mergeAll,
+  props => new Headers(props),
+  r.objOf('headers')
+)
+
 export const get = resource =>
   fetch(getFullResourceName(resource), {
-    headers: new Headers({
-      Authorization: 'Bearer ' + authToken$()
-    })
+    headers: headers(auth())
   }).then(res => res.json())
 
 export const put = r.curry((data, resource) =>
   fetch(getFullResourceName(resource), {
     method: 'PUT',
     body: JSON.stringify(data),
-    headers: new Headers({
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + authToken$()
-    })
+    headers: headers(auth(), sendJson.contentType)
   })
 )
